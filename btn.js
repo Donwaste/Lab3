ec23416b6c21f8f98ab8ba5b187933625557e3dc
Donwaste <164152629+Donwaste@users.noreply.github.com>
2024-04-18 10:05:52 +0300

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function btnController() {
+        var drop = document.querySelector('.drop');
+        var over = document.querySelector('.overlay');
+        drop.classList.toggle('active');
+        over.classList.toggle('active');
+    }
+
+    function removeActive() {
+        var drop = document.querySelector('.drop');
+        var over = document.querySelector('.overlay');
+        drop.classList.remove('active');
+        over.classList.remove('active');
+    }
+
+    document.querySelector('.btn').addEventListener('click', btnController);
+    document.querySelector('.overlay').addEventListener('click', removeActive);
+});
